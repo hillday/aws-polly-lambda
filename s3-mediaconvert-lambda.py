8b8919bed6dd7320ae481f8c2397246a059961c8
hillday <524081959@qq.com>
2022-08-10 16:53:35 +0800

    ffmpeg add

--- s3-mediaconvert-lambda.py
+++ s3-mediaconvert-lambda.py
@@ -104,6 +104,35 @@ def get_json_template():
     
     return template
 
+
+SIGNED_URL_TIMEOUT = 60  
+def test_ffmpeg(bucket,key):
+    error = False
+    client = boto3.client('s3')
+    
+    try:
+      s3_source_signed_url = client.generate_presigned_url('get_object',
+        Params={'Bucket': bucket, 'Key': key},
+        ExpiresIn=SIGNED_URL_TIMEOUT)
+      message = 'Success'
+    except:
+      message = 'Failure'
+      error = True
+    
+    if error :
+        return error, message,None
+    ffprobe = subprocess.run(['/opt/bin/ffprobe', '-loglevel', 'error', '-show_streams', s3_source_signed_url, '-print_format', 'json'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    video_probe = None
+    if ffprobe.returncode == 0:
+      message = 'Success'
+      video_probe = json.loads(ffprobe.stdout.decode('utf-8'))
+      print('video w:h is %s:%s',video_probe['streams'][0]['width'],video_probe['streams'][0]['height'])
+    else:
+      message = 'Failure'
+      error = True
+    
+    return error, message,video_probe
+
 def lambda_handler(event, context):
   
     client = boto3.client('mediaconvert')
